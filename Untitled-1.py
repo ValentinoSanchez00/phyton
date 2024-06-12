baeddf0156d9a79b62a941344f065117453c733c
Valentino <valentinosanchezraverta@gmail.com>
2024-06-12 15:28:16 +0200

    Ejercicio 2

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,3 +1,9 @@
 """ Ej 1 """
-nombre=input("ingrese su nombre \n")
-print("hola " + nombre)
+""" nombre=input("ingrese su nombre \n")
+print("hola " + nombre) """
+
+""" Ej 2 """
+base=int(input("ingrese la base del triángulo \n"))
+altura=int(input("ingrese la altura del triángulo \n"))
+area=base*altura/2
+print("el area del triángulo es: " + str(area))
