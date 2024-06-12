4e71c7c6693918921fa1e6a96648b8f9b04df297
Valentino <valentinosanchezraverta@gmail.com>
2024-06-12 16:10:55 +0200

    Ejercicio 2

--- Alternativas.py
+++ Alternativas.py
@@ -1,5 +1,5 @@
 """ Ejercicio 1 """
-num1= float(input("ingrese el primer numero \n"))
+""" num1= float(input("ingrese el primer numero \n"))
 num2= float(input("ingrese el segundo numero \n"))
 suma=num1+num2
 if suma > 0:
@@ -7,4 +7,12 @@ if suma > 0:
 elif suma < 0:
     print("la suma es negativa")
 else:
-    print("la suma es 0")
+    print("la suma es 0") """
+
+""" Ejercicio 2 """
+num= float(input("ingrese un numero \n"))
+
+if num%2 == 0:
+    print("el numero es par")
+else:
+    print("el numero es impar")
