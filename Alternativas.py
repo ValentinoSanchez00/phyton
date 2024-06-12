960f3a752d37c70feeb73ba44a7d6cec16df9b55
Valentino <valentinosanchezraverta@gmail.com>
2024-06-12 16:16:00 +0200

    Ejercicio 3

--- Alternativas.py
+++ Alternativas.py
@@ -10,9 +10,20 @@ else:
     print("la suma es 0") """
 
 """ Ejercicio 2 """
-num= float(input("ingrese un numero \n"))
+""" num= float(input("ingrese un numero \n"))
 
 if num%2 == 0:
     print("el numero es par")
 else:
-    print("el numero es impar")
+    print("el numero es impar") """
+
+""" Ejercicio 3 """
+mes= int(input("ingrese un numero entero del 1 al 12 \n"))
+if mes == 1 or mes == 3 or mes == 5 or mes == 7 or mes == 8 or mes == 10 or mes == 12:
+    print("el mes tiene 31 dias")
+elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+    print("el mes tiene 30 dias")
+elif mes == 2:
+    print("el mes tiene 28 o 29 dias")
+else:
+    print("el mes no existe")
