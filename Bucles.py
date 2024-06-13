77a117015cdcaa2bd2bd69e9049f144a61392ff5
ValentinoSanchez00 <valentinosanchezraverta@gmail.com>
2024-06-13 08:29:18 +0200

    Ejercicio 2

--- Bucles.py
+++ Bucles.py
@@ -1,7 +1,11 @@
 """ Ejercicio 1 """
-for i in range(1,11):
+""" for i in range(1,11):
     print("Tabla del %i" % i)
     for j in range(1,11):
-     print("%i x %i = %i" % (i, j, j*i))
+     print("%i x %i = %i" % (i, j, j*i)) """
 
 """ Ejercicio 2 """
+año=2001
+while año <= 2024:
+    print("Informe del año ", año)
+    año+=1
